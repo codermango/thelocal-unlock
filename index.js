6a2ccb18f032c49699ce9717ecdb56354075c7e4
Mark <codermango@gmail.com>
2020-06-26 00:34:40 +0200

    Update index.js
    more scure

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 window.addEventListener('load', (event) => {
   console.log('page is fully loaded');
-  const iframeNodeList = document.querySelectorAll("iframe[id^='offer-']");
-  iframeNodeList.forEach(x => x.parentNode.parentNode.remove());
-  document.body.setAttribute('style', 'overflow:scroll !important');
-  document.querySelector(".tp-backdrop.tp-active").remove();
+
+  setTimeout(() => {
+    const elementsWillBeRemoved = [];
+    const iframeNodeList = document.querySelectorAll("iframe[id^='offer-']");
+    const tpBackdrop = document.querySelector(".tp-backdrop.tp-active");
+    const tpModal = document.querySelector(".tp-modal");
+  
+    iframeNodeList.forEach(x => {
+      if (x.parentNode && x.parentNode.parentNode) {
+        elementsWillBeRemoved.push(x.parentNode.parentNode);
+      }
+    });
+  
+    elementsWillBeRemoved.push(tpBackdrop);
+    elementsWillBeRemoved.push(tpModal);
+  
+    elementsWillBeRemoved.forEach(x => {
+      if (x) {
+        x.remove();
+      }
+    });
+  
+    document.body.setAttribute('style', 'overflow:scroll !important');
+  }, 1000);
+
+  
 });
 
 
